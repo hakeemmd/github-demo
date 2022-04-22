0297808673325631a61478b2d1ad12142e2fd002
Hakeem <Hakeem@LAPTOP-8L1HN7SB>
2022-04-22 10:22:50 +0530

    my first commit...

---
+++ src/april22/apr24.java
@@ -0,0 +1,11 @@
+package april22;
+
+public class apr24 {
+
+	public static void main(String[] args) {
+		System.out.println("my name is hakeem");
+		System.out.println("this is wipro family");
+
+	}
+
+}
